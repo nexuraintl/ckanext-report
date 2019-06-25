92fa4666be7606b5e029ce49003e495e2572bdc5
Andres Felipe Orejuela <aorejuela@nexura.com>
2019-06-25 18:05:04 -0500

    Adjustment for translation of texts into Spanish, filtered by date.

--- ckanext/report/fanstatic/javascript/report.js
+++ ckanext/report/fanstatic/javascript/report.js
@@ -1,10 +1,18 @@
-$(document).ready(function()
-    {
-        $("#report-table").tablesorter({
-            dateFormat: "uk",
-        });
+$(document).ready(function () {
         $(".js-auto-submit").change(function () {
             $(this).closest("form").submit();
+        });
+
+        $(".js-btn-summit").on("click", function () {
+            $(this).closest("form").submit();
+        });
+
+        var table = $('#report-table');
+        table.DataTable({
+            searching: false,
+            language: {
+                url: 'https://cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json'
+            }
         });
     }
 );
